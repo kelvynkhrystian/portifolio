e364766939a502981199fcefaedc334125869133
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-02-18 03:18:47 -0300

    apagando-imagens-defeituosas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { HeaderStyles } from '../styles/headerStyles'
+import homeIcon from '../imgs/icons/home.png'
 
 function Header() {
   return (
@@ -14,25 +15,25 @@ function Header() {
       <section>
         <div>
           <Link to="/">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMoeFDPBMcmQo60F8tvmWcut-WrdNfYwr9UzCU-Mw48X3h_peb2p-ed0-VMtmkm1YX5F8&usqp=CAU" alt="icon-test" />
+            <img src={homeIcon} alt="icon-test" />
             <p>Home</p>
           </Link>
         </div>
         <div>
           <Link to="/projects">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMoeFDPBMcmQo60F8tvmWcut-WrdNfYwr9UzCU-Mw48X3h_peb2p-ed0-VMtmkm1YX5F8&usqp=CAU" alt="icon-test" />
+            <img src={homeIcon} alt="icon-test" />
             <p>Projects</p>
           </Link>
         </div>
         <div>
           <Link to="/skills">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMoeFDPBMcmQo60F8tvmWcut-WrdNfYwr9UzCU-Mw48X3h_peb2p-ed0-VMtmkm1YX5F8&usqp=CAU" alt="icon-test" />
+          <img src={homeIcon} alt="icon-test" />
             <p>Skills</p>
           </Link>
         </div>
         <div>
           <Link to="/profile">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMoeFDPBMcmQo60F8tvmWcut-WrdNfYwr9UzCU-Mw48X3h_peb2p-ed0-VMtmkm1YX5F8&usqp=CAU" alt="icon-test" />
+          <img src={homeIcon} alt="icon-test" />
             <p>Profile</p>
           </Link>
         </div>
