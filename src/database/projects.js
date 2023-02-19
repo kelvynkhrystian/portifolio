c2aba508c0684b5809d503991d0ed6d097456469
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-02-19 18:23:42 -0300

    endereços-de-imagem-corretos

--- src/database/projects.js
+++ src/database/projects.js
@@ -3,7 +3,7 @@ const projects = [
     id: 1,
     title: '1º Portifolio',
     subtitle: "primeiro contato com o mundo web",
-    image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
+    image : "https://github.com/kelvynkhrystian/portifolio/blob/main/src/imgs/projects/port1.png?raw=true",
     github: "https://github.com/kelvynkhrystian",
     deploy: "https://github.com/kelvynkhrystian"
   },
@@ -11,7 +11,7 @@ const projects = [
     id: 2,
     title: "Formulário",
     subtitle: "Um forms html sobre pets",
-    image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
+    image : "https://github.com/kelvynkhrystian/portifolio/blob/main/src/imgs/projects/forms1.png?raw=true",
     github: "https://github.com/kelvynkhrystian/formulario",
     deploy: "https://formulario-nine-orcin.vercel.app/"
   },
@@ -19,7 +19,7 @@ const projects = [
     id: 3,
     title: "Login Page",
     subtitle: "Uma página de login - HTML e CSS",
-    image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
+    image : "https://github.com/kelvynkhrystian/portifolio/blob/main/src/imgs/projects/login-page.png?raw=true",
     github: "https://github.com/kelvynkhrystian/login-page",
     deploy: "https://login-page-nine-mocha.vercel.app/"
   },
@@ -27,7 +27,7 @@ const projects = [
     id: 4,
     title: "Calculadora",
     subtitle: "uma calculadora js com tema one piece",
-    image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
+    image : "https://github.com/kelvynkhrystian/portifolio/blob/main/src/imgs/projects/calculadora.png?raw=true",
     github: "https://github.com/kelvynkhrystian/calculadora",
     deploy: "https://calculadora-omega-two.vercel.app/"
   },
@@ -35,7 +35,7 @@ const projects = [
     id: 5,
     title: "Formulário 2",
     subtitle: "Um forms JS com melhorias e validações",
-    image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
+    image : "https://github.com/kelvynkhrystian/portifolio/blob/main/src/imgs/projects/forms2.png?raw=true",
     github: "https://github.com/kelvynkhrystian/formsJS",
     deploy: "https://forms-js.vercel.app/"
   },
@@ -43,7 +43,7 @@ const projects = [
     id: 6,
     title: "To do List",
     subtitle: "Lista de tarefas funcional",
-    image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
+    image : "https://github.com/kelvynkhrystian/portifolio/blob/main/src/imgs/projects/to-do-list.png?raw=true",
     github: "https://github.com/kelvynkhrystian/todolist",
     deploy: "https://todolist-eight-xi.vercel.app/index2.html"
   },
@@ -51,7 +51,7 @@ const projects = [
     id: 7,
     title: "Portifólio 2",
     subtitle: "Mobile First, JS e Responsividade",
-    image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
+    image : "https://github.com/kelvynkhrystian/portifolio/blob/main/src/imgs/projects/port2.png?raw=true",
     github: "https://github.com/kelvynkhrystian/kelvynkhrystian.github.io",
     deploy: "https://kelvynkhrystian.github.io/"
   },
@@ -59,7 +59,7 @@ const projects = [
     id: 8,
     title: "Casa de Câmbio",
     subtitle: "Simula cotação de cambio de uma API",
-    image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
+    image : "https://github.com/kelvynkhrystian/portifolio/blob/main/src/imgs/projects/cambio.png?raw=true",
     github: "https://github.com/kelvynkhrystian/casaDeCambio",
     deploy: "https://casa-de-cambio-nine.vercel.app/"
   },
@@ -67,7 +67,7 @@ const projects = [
     id: 9,
     title: "Shopping Cart",
     subtitle: "carrinho de compras com ES6",
-    image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
+    image : "https://github.com/kelvynkhrystian/portifolio/blob/main/src/imgs/projects/shopping.png?raw=true",
     github: "https://github.com/kelvynkhrystian/shoppingCart",
     deploy: "https://shopping-zeta-jade.vercel.app/"
   },
@@ -75,7 +75,7 @@ const projects = [
     id: 10,
     title: "Lhama Password",
     subtitle: "gerador de senhas em React",
-    image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
+    image : "https://github.com/kelvynkhrystian/portifolio/blob/main/src/imgs/projects/lhama.gif?raw=true",
     github: "https://github.com/kelvynkhrystian/generate-password",
     deploy: "https://generate-password-six.vercel.app/"
   }
