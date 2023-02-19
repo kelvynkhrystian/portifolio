713b9b3c256e470cc59a215e1b0b63d8462d4307
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-02-19 18:19:13 -0300

    colocando-links-corretos-em-github-e-deploy

--- src/database/projects.js
+++ src/database/projects.js
@@ -12,72 +12,72 @@ const projects = [
     title: "Formulário",
     subtitle: "Um forms html sobre pets",
     image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
-    github: "https://github.com/kelvynkhrystian",
-    deploy: "https://github.com/kelvynkhrystian"
+    github: "https://github.com/kelvynkhrystian/formulario",
+    deploy: "https://formulario-nine-orcin.vercel.app/"
   },
   {
     id: 3,
     title: "Login Page",
     subtitle: "Uma página de login - HTML e CSS",
     image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
-    github: "https://github.com/kelvynkhrystian",
-    deploy: "https://github.com/kelvynkhrystian"
+    github: "https://github.com/kelvynkhrystian/login-page",
+    deploy: "https://login-page-nine-mocha.vercel.app/"
   },
   {
     id: 4,
     title: "Calculadora",
     subtitle: "uma calculadora js com tema one piece",
     image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
-    github: "https://github.com/kelvynkhrystian",
-    deploy: "https://github.com/kelvynkhrystian"
+    github: "https://github.com/kelvynkhrystian/calculadora",
+    deploy: "https://calculadora-omega-two.vercel.app/"
   },
   {
     id: 5,
     title: "Formulário 2",
     subtitle: "Um forms JS com melhorias e validações",
     image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
-    github: "https://github.com/kelvynkhrystian",
-    deploy: "https://github.com/kelvynkhrystian"
+    github: "https://github.com/kelvynkhrystian/formsJS",
+    deploy: "https://forms-js.vercel.app/"
   },
   {
     id: 6,
     title: "To do List",
     subtitle: "Lista de tarefas funcional",
     image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
-    github: "https://github.com/kelvynkhrystian",
-    deploy: "https://github.com/kelvynkhrystian"
+    github: "https://github.com/kelvynkhrystian/todolist",
+    deploy: "https://todolist-eight-xi.vercel.app/index2.html"
   },
   {
     id: 7,
     title: "Portifólio 2",
     subtitle: "Mobile First, JS e Responsividade",
     image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
-    github: "https://github.com/kelvynkhrystian",
-    deploy: "https://github.com/kelvynkhrystian"
+    github: "https://github.com/kelvynkhrystian/kelvynkhrystian.github.io",
+    deploy: "https://kelvynkhrystian.github.io/"
   },
   {
     id: 8,
     title: "Casa de Câmbio",
     subtitle: "Simula cotação de cambio de uma API",
     image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
-    github: "https://github.com/kelvynkhrystian",
-    deploy: "https://github.com/kelvynkhrystian"
+    github: "https://github.com/kelvynkhrystian/casaDeCambio",
+    deploy: "https://casa-de-cambio-nine.vercel.app/"
   },
   {
     id: 9,
     title: "Shopping Cart",
     subtitle: "carrinho de compras com ES6",
     image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
-    github: "https://github.com/kelvynkhrystian",
-    deploy: "https://github.com/kelvynkhrystian"
+    github: "https://github.com/kelvynkhrystian/shoppingCart",
+    deploy: "https://shopping-zeta-jade.vercel.app/"
   },
   {
     id: 10,
     title: "Lhama Password",
     subtitle: "gerador de senhas em React",
     image : "https://raw.githubusercontent.com/kelvynkhrystian/portifolio/main/src/imgs/projects/example.webp",
-    github: "https://github.com/kelvynkhrystian",
-    deploy: "https://github.com/kelvynkhrystian"
+    github: "https://github.com/kelvynkhrystian/generate-password",
+    deploy: "https://generate-password-six.vercel.app/"
   }
 
 ];
