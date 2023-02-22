47ab69639d0d41023c8f5a7f48a09a4a8d65b0c1
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-02-22 18:55:18 -0300

    iniciando-db-skills

---
+++ src/database/skills.js
@@ -0,0 +1,16 @@
+const skills = [
+  {
+    id: 1,
+    title: 'HTML',
+    subtitle: "HTML é uma linguagem de marcação utilizada na construção de páginas na Web.",
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpngGRjYX1ca7qAADU3K6eGLj7ShQE3L2otdzfryl_Y9Ht2QRoQKYQbsXd36XIxMbYOw0&usqp=CAU",
+  },
+  {
+    id: 2,
+    title: 'CSS',
+    subtitle: "Cascading Style Sheets é um mecanismo para adicionar estilos a uma página web",
+    image : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png",
+  }
+]
+
+export default skills;
