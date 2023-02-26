ae683988fe7c8d70164fdceb3cc047fc401fe64b
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-02-26 16:56:54 -0300

    refatorando-notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from 'react';
+import Header from '../components/Header';
 
 function NotFound() {
   return (
     <>
-        <Header />
-        <div>NotFound</div>
+      <Header />
+      <h1>404 - Página não encontrada</h1>
+      <div>Desculpe, a página que você está procurando não existe.</div>
     </>
-    
-  )
+  );
 }
 
-export default NotFound
+export default NotFound;
